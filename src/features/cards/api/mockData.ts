3a36fbd5cf815f21f3158c5992fed99486e9b188
davewaldorf <dave.waldorf@me.com>
2024-12-31 17:28:48 +0100

    feat: added mock API for card data and mock card entries

---
+++ src/features/cards/api/mockData.ts
@@ -0,0 +1,30 @@
+export const mockCards = [
+  {
+    id: 1,
+    balance: "$5,756",
+    cardHolder: "Eddy Cusuma",
+    cardNumber: "3778 **** **** 1234",
+    validThru: "12/22",
+  },
+  {
+    id: 2,
+    balance: "$2,340",
+    cardHolder: "Jemi Wilson",
+    cardNumber: "4111 **** **** 5678",
+    validThru: "05/24",
+  },
+  {
+    id: 3,
+    balance: "$1,450",
+    cardHolder: "Michael Scott",
+    cardNumber: "4111 **** **** 7890",
+    validThru: "08/25",
+  },
+  {
+    id: 4,
+    balance: "$7,100",
+    cardHolder: "Pam Beesly",
+    cardNumber: "3778 **** **** 4567",
+    validThru: "01/26",
+  },
+];
