f676938786a5a6217f071f6eca3ca6479b9306ba
davewaldorf <dave.waldorf@me.com>
2024-12-31 01:05:59 +0100

    feat: implement routing with AppRouter component and define route paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
 
-import Settings from "./pages/Settings";
-import Dashboard from "./pages/Dashboard";
+import AppRouter from "./routes/AppRouter";
 
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import { theme } from "./styles/theme";
-import Layout from "./components/Layout/Layout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </Layout>
-      </Router>
+        <AppRouter />
     </ThemeProvider>
   </StrictMode>
 );
