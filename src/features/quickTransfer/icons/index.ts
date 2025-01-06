0b8b3c04b348093be9f4e8e14b8394d28da32bc2
davewaldorf <dave.waldorf@me.com>
2025-01-06 21:14:05 +0100

    feat: icons added

---
+++ src/features/quickTransfer/icons/index.ts
@@ -0,0 +1,22 @@
+import livia_bator from './livia_bator.jpeg';
+import randy_press from './randy_press.jpeg';
+import workman from './workman.jpeg';
+import livia_bator_2 from './livia_bator_2.jpeg';
+import randy_press_2 from './randy_press_2.jpeg';
+import workman_2 from './workman_2.jpeg';
+import arrowBack from './arrowBack.svg';
+import arrowNext from './arrowNext.svg';
+import paperPlane from './paperPlane.svg';
+
+
+export default {
+  livia_bator,
+  randy_press,
+  workman,
+  livia_bator_2,
+  randy_press_2,
+  workman_2,
+  arrowBack,
+  arrowNext,
+  paperPlane,
+};
