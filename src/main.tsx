4c748ec149142d0c847cd83c6d80af04096ac3fc
davewaldorf <dave.waldorf@me.com>
2025-01-09 21:55:56 +0100

    feat: integrated Redux Provider into main application entry point

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,16 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import { theme } from "./styles/theme";
 
+import { Provider } from "react-redux";
+import store from "./store/store";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
         <AppRouter />
-    </ThemeProvider>
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
