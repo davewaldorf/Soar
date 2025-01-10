572902019fe12663a3ba64a71f56fb9b66ce47b5
davewaldorf <dave.waldorf@me.com>
2025-01-10 10:41:57 +0100

    feat: implemented async thunk for fetching cards and manage loading state

--- src/store/slices/cardsSlice.ts
+++ src/store/slices/cardsSlice.ts
@@ -1,23 +1,41 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { Card } from '../types/cardsTypes';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { CardType, fetchCards } from '../../features/cards/api/fakeApi';
 
 interface CardsState {
-  cards: Card[];
+  cards: CardType[];
+  loading: boolean;
 }
 
-const initialCardsState: CardsState = {
+const initialState: CardsState = {
   cards: [],
+  loading: false,
 };
+
+export const fetchCardsThunk = createAsyncThunk<CardType[]>(
+  'cards/fetchCards',
+  async () => {
+    const response = await fetchCards();
+    return response;
+  }
+);
 
 const cardsSlice = createSlice({
   name: 'cards',
-  initialState: initialCardsState,
-  reducers: {
-    setCards(state, action: PayloadAction<Card[]>) {
-      state.cards = action.payload;
-    },
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCardsThunk.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchCardsThunk.fulfilled, (state, action) => {
+        state.cards = action.payload;
+        state.loading = false;
+      })
+      .addCase(fetchCardsThunk.rejected, (state) => {
+        state.loading = false;
+      });
   },
 });
 
-export const { setCards } = cardsSlice.actions;
 export default cardsSlice.reducer;
